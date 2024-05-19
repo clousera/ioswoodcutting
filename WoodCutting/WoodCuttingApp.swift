665951a0333f27c07becb6eada72b85265ed46ed
AmandaClouser <aclouser@michiganlabs.com>
2024-05-18 21:19:11 -0400

    Initial Commit

---
+++ WoodCutting/WoodCuttingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodCuttingApp.swift
+//  WoodCutting
+//
+//  Created by Amanada Clouser on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WoodCuttingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
